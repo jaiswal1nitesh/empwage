8bfe3379e31bc8acd97b486dfc5d16fa546b32d4
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-24 12:04:25 +0530

    [Nitesh] : Add check Employee Present or Absent

--- empWage_comput.sh
+++ empWage_comput.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo " Welcome to the employee Wage Computation "
+checkEmpPresent(){
+case  $1 in
+ 1)  echo " Employee is Present " ;;
+ 0) echo " Employee is Absent " ;;
+esac
+}
+checkEmpPresent $(($RANDOM%2))
+  

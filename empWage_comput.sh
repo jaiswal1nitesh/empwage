58447a1738c2d55b8171bd4681597d5a674b86ab
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-24 14:07:27 +0530

    [Nitesh] Add : Part time emplyoee is added

--- empWage_comput.sh
+++ empWage_comput.sh
@@ -2,14 +2,20 @@
 echo " ------------------>Welcome to the employee Wage Computation<---------------- "
 echo
 empWagePerHour=20
-empWorkHour=8
-randomCheck=$(($RANDOM%2))
+empFullTimeWorkHour=8
+empParttimePerHour=4
+randomCheck=$(($RANDOM%3))
 empPresent=1
+empPartTime=2
 empAbsent=0
 if [ $empPresent == $randomCheck ] 
  then 
 	echo " Employee is Present " 
-	let salary=empWagePerHour*empWorkHour
+	let salary=empWagePerHour*empFullTimeWorkHour
+elif [ $empPartTime == $randomCheck ]
+then
+	echo " Employee is PartTime "
+	let salary=empWagePerHour*empParttimePerHour
 else 
 	echo " Employee is Absent "
 	let salary=empWagePerHour*empAbsent

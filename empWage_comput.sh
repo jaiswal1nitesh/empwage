289666836d07b79037df77498c6a88d5ad796469
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-24 12:53:48 +0530

    Add [Nitsh] calulate daily Employee wage

--- empWage_comput.sh
+++ empWage_comput.sh
@@ -1,25 +1,17 @@
 #!/bin/bash -x
-echo " Welcome to the employee Wage Computation "
+echo " ------------------>Welcome to the employee Wage Computation<---------------- "
+echo
 empWagePerHour=20
 empWorkHour=8
-checkEmpPresent(){
-case  $1 in
- 1)  echo " Employee is Present " 
-     let  salary=empWagePerHour*empWorkHour
-     ;;
- 0) echo " Employee is Absent " 
-    let  salary=empWageHour*empWorkHour
-     ;;
-esac
-}
-checkEmpPresent $(($RANDOM%2))
-echo  " Employee Daily Wage $salary"
 randomCheck=$(($RANDOM%2))
 empPresent=1
 empAbsent=0
 if [ $empPresent == $randomCheck ] 
  then 
-	echo " Employee is Present "
+	echo " Employee is Present " 
+	let salary=empWagePerHour*empWorkHour
 else 
 	echo " Employee is Absent "
+	let salary=empWagePerHour*empAbsent
 fi
+echo " Employee salary is $salary "
